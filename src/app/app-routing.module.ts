d6863aa8c4060ebf4a6a9b703b4c2c2772b31b1a
JA <jose.albelt1817@gmail.com>
2023-11-06 20:26:13 -0500

    menu router-link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SedeComponent } from './componets/sede/sede.component';
+import { ClaseComponent } from './componets/clase/clase.component';
+import { EntrenadoresComponent } from './componets/entrenadores/entrenadores.component';
+import { ProductoComponent } from './componets/producto/producto.component';
+
+const routes: Routes = [
+  {path:'',redirectTo: 'sede', pathMatch:'full'},
+  {path:'sede',component:SedeComponent},
+  {path:'producto',component:ProductoComponent},
+  {path:'clase',component:ClaseComponent},
+  {path:'entrenador',component:EntrenadoresComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { SedeComponent } from './componets/sede/sede.component';
+import { ProductoComponent } from './componets/producto/producto.component';
+import { ClaseComponent } from './componets/clase/clase.component';
+import { EntrenadoresComponent } from './componets/entrenadores/entrenadores.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent,
-    FooterComponent
+    FooterComponent,
+    SedeComponent,
+    ProductoComponent,
+    ClaseComponent,
+    EntrenadoresComponent
   ],
   imports: [
     BrowserModule,
