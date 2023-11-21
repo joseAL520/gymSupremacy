ecac9814889aa68e7bb25bc062aec438d002e0d1
JA <jose.albelt1817@gmail.com>
2023-11-20 20:34:38 -0500

    funcion del login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
 
 
+  onSubmit() { 
+    if (this.username === 'admin' && this.password === 'admin') {
+     
+      alert('Nombre de usuario o contraseña correctos');
+      this.router.navigate(['/clase']);
+    } else {
+      alert('Nombre de usuario o contraseña incorrectos');
+    }
+
+
+  }
   
 }
